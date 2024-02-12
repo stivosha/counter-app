34ee38c6e21dad8f997cd924216f1d2751950a44
stivosha <gosha75217521@gmail.com>
2024-02-12 18:38:44 +0500

    init

---
+++ app/src/main/java/com/stivosha/counter_app/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.stivosha.counter_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
